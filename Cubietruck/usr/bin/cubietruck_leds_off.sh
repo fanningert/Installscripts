cf052301809f115634998ba49957aaa09b051fcb
Thomas Fanninger <thomas@fanninger.at>
2014-09-06 23:26:21 +0200

    Add systemd service for disable leds

---
+++ Cubietruck/usr/bin/cubietruck_leds_off.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo 0 > /sys/class/leds/blue\:ph21\:led1/brightness
+echo 0 > /sys/class/leds/orange\:ph20\:led2/brightness
+echo 0 > /sys/class/leds/white\:ph11\:led3/brightness
+echo 0 > /sys/class/leds/green\:ph07\:led4/brightness
